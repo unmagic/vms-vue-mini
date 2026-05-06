1baf506fc482a2072ceacf19ba4126ae32e9c01c
liubiao <1178545208@qq.com>
2026-05-06 17:07:36 +0800

    test(core): 移除 instance.spec.ts 中全局 Page 模拟实现
    - 删除了全局 Page 函数的模拟定义及相关辅助变量
    - 精简了 tests/core/__tests__/instance.spec.ts 中无用代码
    - 保持现有测试逻辑，减少全局污染
    - 使测试代码更简洁维护更方便

--- packages/core/__tests__/instance.spec.ts
+++ packages/core/__tests__/instance.spec.ts
@@ -38,42 +38,6 @@ globalThis.Component = (options: Record<string, any>) => {
   }
 }
 
-let page: Record<string, any>
-// @ts-expect-error
-globalThis.Page = (options: Record<string, any>) => {
-  page = {
-    ...options,
-    is: '',
-    route: '',
-    options: {},
-    createSelectorQuery() {},
-    createIntersectionObserver() {},
-    createMediaQueryObserver() {},
-    selectComponent() {},
-    selectAllComponents() {},
-    getTabBar() {},
-    getPageId() {},
-    animate() {},
-    clearAnimation() {},
-    getOpenerEventChannel() {},
-    applyAnimatedStyle() {},
-    clearAnimatedStyle() {},
-    setUpdatePerformanceListener() {},
-    getPassiveEvent() {},
-    setPassiveEvent() {},
-    setInitialRenderingCache() {},
-    setData(data: Record<string, unknown>) {
-      this.data = this.data || {}
-      Object.keys(data).forEach((key) => {
-        this.data[key] = data[key]
-      })
-      if (component) {
-        component.lifetimes.attached.call(component)
-      }
-    },
-  }
-}
-
 describe('instance', () => {
   test('unset current instance', () => {
     const fn = vi.fn()
@@ -82,8 +46,6 @@ describe('instance', () => {
       onShow(fn)
     })
 
-    page.onLoad()
-    page.onShow()
     expect(fn).toHaveBeenCalledTimes(0)
 
     component.pageLifetimes.show.call(component)
